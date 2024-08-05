566c3914b609def579dd01cd2406bc59f98939cb
galkesten <71960468+galkesten@users.noreply.github.com>
2024-08-05 18:19:30 +0300

    Update answers.py

--- Homework3/hw3/answers.py
+++ Homework3/hw3/answers.py
@@ -47,17 +47,55 @@ def part1_generation_params():
 
 
 part1_q1 = r"""
-**Your answer:**
+We split the corpus due to the following reasons:
+
+1)**Preventing Vanishing and Exploding Gradients:** 
+During the training of RNNs, backpropagation through time (BPTT) is used to update weights. 
+If the sequences are too long, the gradients can either vanish or explode, leading to training difficulties 
+and numeric instability. Splitting the text into shorter sequences helps mitigate this issue by limiting the
+number of time steps over which gradients need to be propagated, thus stabilizing the training process.
+
+2)**Memory Limits:** Since RNNs process each token sequentially, we need to keep the hidden state for each time step until we perform backpropagation. If the sequences are too long, the memory consumption can become excessive, potentially exceeding available resources. Splitting the corpus into shorter sequences helps manage memory usage more efficiently.
+
+3 )**Batch Processing:** Even though RNNs process tokens sequentially within each sample, 
+dividing the corpus into smaller sequences allows us to process tokens from different samples in
+parallel within a batch. This parallel processing enhances computational efficiency, making the training process
+faster.
+
+4)**Focusing on Local Context:** For many language tasks, the local context (nearby words or tokens) is
+most relevant for making predictions. Splitting the text into sequences allows the RNN to focus on learning from
+these local contexts, making it easier to model short-term dependencies that are crucial for tasks like sequence
+prediction.
+
+
 
 """
 
 part1_q2 = r"""
-**Your answer:**
+During training, the model processes sequences of a limited length,
+grouped into batches. Each sample in a batch continues in the next batch,
+meaning that sample I in batch j continues as sample I in batch j+1. This setup ensures that the model's
+hidden state, which captures the learned context, is transferred from one batch to the next. 
+As a result, the model retains memory across batches, allowing it to understand a longer context than
+the sequence length might suggest. Even though the optimization happens within the scope of each sequence,
+the continuity provided by the hidden state transfer means the model learns from a broader context. 
+During inference, this allows the model to generate meaningful longer sequences.
+The batch size used during training influences how context is preserved across sequences.
+A larger batch size tends to break the corpus into smaller, more isolated segments, as each position
+in the batch retains its individual memory and context. In contrast, a smaller batch size allows the model to
+carry context over longer sections of text,
+as it processes more batches and maintains continuity across them.
 
 """
 
 part1_q3 = r"""
-**Your answer:**
+We are not shuffling the order of batches because each sample in a batch continues
+into the next batch. Ensuring that batches follow one another in the correct order allows the model
+to transfer the hidden state from one batch to the next, preserving the learned context across batches.
+This continuity enables the model to understand longer sequences than the individual sequence length might suggest.
+Shuffling the batches would disrupt this continuity, preventing the model from effectively learning the relationships
+between consecutive elements and undermining its ability
+to retain and utilize long-term dependencies.
 
 """
 
